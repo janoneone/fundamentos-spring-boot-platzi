8dbdc81fde3bbdcedbe426efed107d55afa9174c
Alejandro Castro <acastro.ma1@gmail.com>
2022-06-21 20:41:22 -0400

    segundo commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/MyConfigurationBean.java
@@ -0,0 +1,15 @@
+package com.fundamentosplatzi.springboot.fundamentos.configuration;
+
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanImplement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfigurationBean {
+    @Bean
+    public MyBean beanOperation(){
+        return new MyBeanImplement();
+    }
+
+}
